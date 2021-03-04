ba5fb11648b2dd1aae6cb76df9062a604f0af5c6
Fabián Sánchez León <fadarsaleeing@gmail.com>
2021-03-04 17:12:39 -0500

    update logo

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -4,7 +4,7 @@ export const environment = {
     autenticacion: true,
     notificaciones: false,
     menuApps: true,
-    appname: 'sga',
+    appname: 'alternancia',
     appMenu: 'alternancia',
     NUXEO: {
         PATH: 'https://documental.udistrital.edu.co/nuxeo/',
