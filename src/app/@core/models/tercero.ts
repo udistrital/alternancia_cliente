9ede01dca6bf085315bd59682bb37fb38e9cb627
cristian <cryteck_1011@hotmail.com>
2021-03-08 17:40:10 -0500

    feat: junto con @JocoolQ se agregan modelos de datos para formularios de informacion basica #2

---
+++ src/app/@core/models/tercero.ts
@@ -0,0 +1,14 @@
+export class Tercero{
+  Id: number;
+  NombreCompleto: string;
+  PrimerNombre: string;
+  SegundoNombre: string;
+  PrimerApellido: string;
+  SegundoApellido: string;
+  LugarOrigen:  number;
+  FechaNacimiento: string;
+  Activo: boolean;
+  FechaCreacion: string;
+  FechaModificacion: string;
+  UsuarioWSO2: string;
+}
--- src/app/pages/informacion-basica/informacion-basica.component.ts
+++ src/app/pages/informacion-basica/informacion-basica.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { RequestManager } from '../services/requestManager';
+import Swal from 'sweetalert2';
 import { UserService } from '../services/userService';
+import { Tercero } from '../../@core/models/tercero';
+import { DatosIdentificacion } from '../../@core/models/datos_identificacion';
 import { environment } from './../../../environments/environment' 
 @Component({
   selector: 'app-informacion-basica',
@@ -8,24 +11,46 @@ import { environment } from './../../../environments/environment'
   styleUrls: ['./informacion-basica.component.scss']
 })
 export class InformacionBasicaComponent implements OnInit {
-  terceros: any;
+  tercero: Tercero;
+  loaded: boolean = false;
+  datosIdentificacion: DatosIdentificacion;
   constructor(
     private request: RequestManager,
     private userService: UserService
-    ) {
+  ) {
 
-
-  }
+    }
 
   ngOnInit(): void {
+    Swal.fire({
+      title: 'Por favor espere!',
+      html: 'Cargando información del recurso ...',
+      allowOutsideClick: false,
+      onBeforeOpen: () => {
+        Swal.showLoading()
+      },
+    });
     
-    this.userService.user$.subscribe((data)=> {
-      console.log("tercero",  data)
-    })
-    this.request.get(environment.TERCEROS_SERVICE,`tercero` )
-    .subscribe((res: any) => {
-      this.terceros = res;
-    })
+    
+      this.userService.user$.subscribe((data)=> {
+        console.log("tercero",  data)
+      })
+      this.request.get(environment.TERCEROS_SERVICE,`tercero/?query=UsuarioWSO2:jgcastellanosj` )
+      .subscribe((res: any) => {
+        this.tercero = res[0];
+        
+        this.request.get(environment.TERCEROS_SERVICE,`datos_identificacion/?query=TerceroId.Id:`+ this.tercero.Id)
+        .subscribe((res: any) => {
+          this.datosIdentificacion = res[0];        
+          Swal.close();
+          this.loaded = true;
+        })
+
+      })
+      
+
+      
+    
   }
 
 }
